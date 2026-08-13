0cf43dcc10989e83202c90073a8fee2d6a9ef89d
Omoshola <heylehgbedey@gmail.com>
2026-08-13 07:11:29 -0700

    feat: implement database setup with docker and manual migration

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+function checkRequiredEnvVariable(key: string): string {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Missing env variables for ${key}`);
+  }
+
+  return value;
+}
+
 export const env = {
   port: Number(process.env.PORT) || 4000,
   isProduction: (process.env.NODE_ENV ?? "development") === "production",
   nodeEnv: process.env.NODE_ENV ?? "development",
   logLevel: process.env.LOG_LEVEL,
   allowOrigin: process.env.ALLOWED_ORIGINS,
+  dbUrl: checkRequiredEnvVariable("DATABASE_URL"),
 } as const;
