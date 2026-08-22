b41df74597613bf34cf75c6b159b2542a9dfe9b5
Omoshola <heylehgbedey@gmail.com>
2026-08-22 11:43:29 -0700

    feat(user): implement redis-caching on user api endpoints and cache invalidation on (create, update, and delete) operation

--- src/services/user.task.service.ts
+++ src/services/user.task.service.ts
@@ -1,4 +1,13 @@
+import { env } from "../config/env";
 import { AppError } from "../lib/appError";
+import {
+  createCacheKeyWithUserIdAndTaskId,
+  createUserTaskListCacheKey,
+  invalidateAdminCache,
+  invalidateTaskCachesForAdminAndUser,
+  invalidateUserTaskCache,
+} from "../lib/cache-helper";
+import { redisClient } from "../redis/redis";
 import {
   createTask,
   deleteTask,
@@ -8,6 +17,8 @@ import {
 } from "../repositories/user.task.respository";
 import { Task } from "../types";
 
+const CACHE_EXPIRY_TIME = Number(env.cache_expiry);
+
 function validateTitle(title: unknown): string {
   if (typeof title !== "string") {
     throw new AppError(400, "Invalid title property");
@@ -16,25 +27,62 @@ function validateTitle(title: unknown): string {
   const trimmedTitle = title.trim();
 
   if (trimmedTitle.length > 100) {
-    throw new AppError(400, "Title can't be more that 100 characters");
+    throw new AppError(400, "Title can't be more than 100 characters");
   }
 
   return trimmedTitle;
 }
 
 export async function getUserTasks(userId: string): Promise<Task[]> {
-  return getTaskByUserId(userId);
+  const USER_CACHED_TASKLISTS = createUserTaskListCacheKey(userId);
+
+  const userCachedTaskList = await redisClient.get(USER_CACHED_TASKLISTS);
+
+  if (userCachedTaskList) {
+    return JSON.parse(userCachedTaskList) as Task[];
+  }
+
+  const tasks = await getTaskByUserId(userId);
+
+  if (!tasks.length) {
+    throw new AppError(
+      400,
+      "No tasks for this user yet. Proceed to create task...",
+    );
+  }
+
+  await redisClient.setEx(
+    USER_CACHED_TASKLISTS,
+    CACHE_EXPIRY_TIME,
+    JSON.stringify(tasks),
+  );
+
+  return tasks;
 }
 
 export async function getUserTaskById(
   userId: string,
   taskId: string,
 ): Promise<Task> {
+  const USER_CACHED_TASK = createCacheKeyWithUserIdAndTaskId(userId, taskId);
+
+  const cachedUserTask = await redisClient.get(USER_CACHED_TASK);
+
+  if (cachedUserTask) {
+    return JSON.parse(cachedUserTask) as Task;
+  }
+
   const task = await getTaskById(userId, taskId);
 
   if (!task) {
-    throw new AppError(404, "task not found");
+    throw new AppError(404, "Task not found");
   }
+
+  await redisClient.setEx(
+    USER_CACHED_TASK,
+    CACHE_EXPIRY_TIME,
+    JSON.stringify(task),
+  );
 
   return task;
 }
@@ -45,7 +93,14 @@ export async function createUserTask(
 ): Promise<Task> {
   const validatedTitle = validateTitle(title);
 
-  return createTask(userId, validatedTitle);
+  const newTask = await createTask(userId, validatedTitle);
+
+  await Promise.all([
+    invalidateAdminCache(), 
+    invalidateUserTaskCache(userId)
+  ]);
+
+  return newTask;
 }
 
 export async function updateUserTask(
@@ -55,11 +110,13 @@ export async function updateUserTask(
 ): Promise<Task> {
   const valiatedTitle = validateTitle(title);
 
-  const task = updateTask(userId, valiatedTitle, taskId);
+  const task = await updateTask(userId, valiatedTitle, taskId);
 
   if (!task) {
-    throw new AppError(404, "task not found");
+    throw new AppError(404, "Task not found");
   }
+
+  await invalidateTaskCachesForAdminAndUser(userId, taskId);
 
   return task;
 }
@@ -71,6 +128,8 @@ export async function deleteUserTask(
   const deleted = await deleteTask(taskId, userId);
 
   if (!deleted) {
-    throw new AppError(404, "task not found");
+    throw new AppError(404, "Task not found");
   }
+
+  await invalidateTaskCachesForAdminAndUser(userId, taskId);
 }
